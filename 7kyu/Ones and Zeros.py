dfa9229a7bb9338a219ce57867fc246aed778206
Nea <mintbluenea@gmail.com>
2023-03-22 00:26:31 +0100

    Update Ones and Zeros.py

--- 7kyu/Ones and Zeros.py
+++ 7kyu/Ones and Zeros.py
@@ -1,4 +1,4 @@
-#############################
+############################
 # Given an array of ones and zeroes, convert the equivalent binary value to an integer.
 
 # Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
@@ -14,7 +14,7 @@
 # Testing: [1, 1, 1, 1] ==> 15
 # Testing: [1, 0, 1, 1] ==> 11
 # However, the arrays can have varying lengths, not just limited to 4.
-#############################
+############################
 
 
 def binary_array_to_number(arr):
